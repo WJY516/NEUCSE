66773565f5dcfe7441533e367fd20540a34f5d83
WJY516 <wujiayi0924@qq.com>
2017-07-25 15:13:33 +0800

    NewsPush V1.1

--- NewsPush/models.py
+++ NewsPush/models.py
@@ -4,31 +4,41 @@ from django.db import models
 from django.contrib import admin
 from ckeditor_uploader.fields import RichTextUploadingField
 from django.contrib.auth.models import User
+from django.utils import timezone
 from django_fsm import FSMField, transition, can_proceed
 
 
 
 #Create your models here.
 
-STATUS_CHOICES = (
-    ('d', 'Draft'),
-    ('p', 'Published'),
-    ('w', 'Withdrawn'),
-)
-
-
+class PublishedManager(models.Manager):
+   def get_queryset(self):
+       return super(PublishedManager,self).get_queryset().filter(status='p')
 
 class NewsPost(models.Model):
+    STATUS_CHOICES = (
+        ('d', 'Draft'),
+        ('p', 'Published'),
+        ('w', 'Withdrawn'),
+    )
     title = models.CharField(max_length=100, verbose_name="标题")
-    author = models.CharField(max_length=50, default=" ",verbose_name="作者")
+    author = models.ForeignKey(User, related_name='newspost')
     cover = models.ImageField(upload_to='Img', verbose_name="封面图")
     body = RichTextUploadingField(blank=True, null=True, verbose_name="内容")
-    timestamp = models.DateTimeField(verbose_name="发布时间")
+    timestamp = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True,null=True)
+    updated = models.DateTimeField(auto_now=True)
     headline = models.CharField(max_length=50, default=" ", verbose_name="摘要")
     status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='d',verbose_name="状态")
 
     def __unicode__(self):
         return self.title
+
+    class Meta:
+        ordering = ('-timestamp',)
+
+    objects = models.Manager()  # The default manager
+    published = PublishedManager()  # Our custom manager
 
 def make_published(ModelAdmin, request, queryset):
     queryset.update(status='p')
--- NewsPush/views.py
+++ NewsPush/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
@@ -11,16 +11,14 @@ from treeView.models import treeView
 
 def index(request):
     # news_list = models.NewsPost.objects.all()
-    news_list = models.NewsPost.objects.filter(status='p')
+    # news_list = models.NewsPost.objects.filter(status='p')
+    news_list = models.NewsPost.published.all()
     nodes = treeView.objects.all()
     return render(request,'NewsPush/index.html', {'news_list': news_list, 'nodes': nodes})
 
 
 def news_content_page(request,news_list_id):
     news = models.NewsPost.objects.get(pk=news_list_id)
-    if news.status != 'p':
-        news = models.NewsPost.objects.none()
-        return HttpResponse('403')
     nodes = treeView.objects.all()
     return render(request,'NewsPush/news_content_page.html',{'news': news, 'nodes': nodes})
 
