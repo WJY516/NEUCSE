799e6c284d3b612eaa063cc1dc7ffd6e3c3ec362
WJY516 <wujiayi0924@qq.com>
2017-08-04 22:49:16 +0800

    temp3

--- online/admin.py
+++ online/admin.py
@@ -2,36 +2,38 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import *
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.forms import  UserCreationForm, UserChangeForm
+
+from .models import UserProfile
 
 # Register your models here.
 
 # 重新定义增加用户表单，继承UserCreationForm
-class MyUserCreationForm(UserCreationForm):
-    def __init__(self, *args, **kwargs):
-        super(MyUserCreationForm, self).__init__(*args, **kwargs)
-        self.fields['name'].required = True
-
-
-# 重新定义编辑用户表单，继承UserChangeForm
-class MyUserChangeForm(UserChangeForm):
-    def __init__(self, *args, **kwargs):
-        super(MyUserChangeForm, self).__init__(*args, **kwargs)
-        self.fields['name'].required = True
-
-
-class CustomUserAdmin(UserAdmin):
-    def __init__(self, *args, **kwargs):
-        super(CustomUserAdmin, self).__init__(*args, **kwargs)
-        self.list_display = ('username', 'is_active', 'is_staff', )
-        self.search_fields = ('username',)
-        self.form = MyUserChangeForm #使用自定义表单
-        self.add_form = MyUserCreationForm
-
-admin.site.register(UserProfile,CustomUserAdmin)
+# class MyUserCreationForm(UserCreationForm):
+#     def __init__(self, *args, **kwargs):
+#         super(MyUserCreationForm, self).__init__(*args, **kwargs)
+#         self.fields['name'].required = True
+#
+#
+# # 重新定义编辑用户表单，继承UserChangeForm
+# class MyUserChangeForm(UserChangeForm):
+#     def __init__(self, *args, **kwargs):
+#         super(MyUserChangeForm, self).__init__(*args, **kwargs)
+#         self.fields['name'].required = True
+#
+#
+# class CustomUserAdmin(UserAdmin):
+#     def __init__(self, *args, **kwargs):
+#         super(CustomUserAdmin, self).__init__(*args, **kwargs)
+#         self.list_display = ('username', 'is_active', 'is_staff', )
+#         self.search_fields = ('username',)
+#         self.form = MyUserChangeForm #使用自定义表单
+#         self.add_form = MyUserCreationForm
+#
+# admin.site.register(UserProfile,CustomUserAdmin)
 
 # class UserAdmin(admin.ModelAdmin):
 #     list_display = ('username', 'is_active',)
@@ -39,3 +41,32 @@ admin.site.register(UserProfile,CustomUserAdmin)
 #
 #
 # admin.site.register(UserProfile,UserAdmin)
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'UserProfile'
+    fk_name = 'user'
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (UserProfileInline, )
+    list_display = ('username', 'is_active', 'is_staff', 'get_group',)
+    list_select_related = ('userprofile', )
+    list_editable = ('is_active', 'is_staff',)
+
+    def get_group(self,instance):
+        return instance.userprofile.usergroup
+    get_group.short_description = 'rank'
+
+    def get_groups(self,obj):
+        return self.groups
+    # get_group.short_description = 'group'
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
--- online/models.py
+++ online/models.py
@@ -2,48 +2,51 @@
 from __future__ import unicode_literals
 from django.contrib import admin
 from django.db import models
-from django.contrib.auth.models import Group, AbstractUser
+from django.contrib.auth.models import Group, User
 from django.db.models.signals import post_save
 from django.utils import timezone
+from django.dispatch import receiver
 
-class UserProfile(AbstractUser):
-    # GROUP_CHOICES = (
-    #     ('0', u'未分配'),
-    #     ('1', u'学生'),
-    #     ('2', u'老师'),
-    #     ('3', u'领导'),
-    # )
-    # user = models.CharField(max_length=32, null=False, blank=False, )
+class UserProfile(models.Model):
+    USERGROUP_CHOICES = (
+        (0, u'未分配'),
+        (1, u'学生'),
+        (2, u'老师'),
+        (3, u'领导'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     # user_is_staff = models.BooleanField(default=False)
     # user_is_active = models.BooleanField(default=True,verbose_name="激活")
-    # usergroup = models.CharField(max_length=1, choices=GROUP_CHOICES, default='0',verbose_name="群组")
+    usergroup = models.PositiveSmallIntegerField(choices=USERGROUP_CHOICES, default='0', verbose_name="群组")
     # useremail = models.EmailField (max_length =75)
-    CardID = models.CharField(max_length=200,blank=True)
-    username = models.CharField(
-        max_length=150,
-        unique=True,
-        default=""
-    )
-    first_name = models.CharField(max_length=30, blank=True, default=" ")
-    last_name = models.CharField(max_length=30, blank=True, default=" ")
-    email = models.EmailField(blank=True, default=" ")
-    is_staff = models.BooleanField(default=False,)
-    is_active = models.BooleanField(default=True,)
-    password = models.CharField(max_length=32,default=" ")
-    date_joined = models.DateTimeField(default=timezone.now)
+    # CardID = models.CharField(max_length=200,blank=True)
+    # username = models.CharField(
+    #     max_length=150,
+    #     unique=True,
+    #     default=""
+    # )
+    # first_name = models.CharField(max_length=30, blank=True, default=" ")
+    # last_name = models.CharField(max_length=30, blank=True, default=" ")
+    # email = models.EmailField(blank=True, default=" ")
+    # is_staff = models.BooleanField(default=False,)
+    # is_active = models.BooleanField(default=True,)
+    # password = models.CharField(max_length=32,default=" ")
+    # date_joined = models.DateTimeField(default=timezone.now)
     # class Meta(AbstractUser.Meta):
     #     swappable = 'AUTH_USER_MODEL'
     #     abstract = False
 
+    def __str__(self):
+        return self.user.username
 
 
 
+@receiver(post_save, sender=User)
+def create_user_profile(sender,instance,created,**kwags):
+    if created:
+        UserProfile.objects.create(user=instance)
 
-# def create_user_profile(sender,instance,created,**kwags):
-#     if created:
-#         UserProfile.objects.create(user=instance)
-#
-# post_save.connect(create_user_profile,sender=User)
+post_save.connect(create_user_profile,sender=User)
 
 
 
